341eec051cf433ec2b59b785629b379a04da4962
Espinal <Haespina@my.loyno.edu>
2019-07-15 09:27:24 -0700

    get users posts and albums

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-$("#login").click (function(){
+$("#login").click(function(){
 	fetch('https://jsonplaceholder.typicode.com/users?username=' + $('#username').val())
 		.then(function(response){
 			return response.json();
@@ -10,8 +10,32 @@ $("#login").click (function(){
 			$("#homepage").show();
 
 			$("#name").html(user.name);
+
+			return fetch('https://jsonplaceholder.typicode.com/posts?userId=' + user.id);
+		})
+		.then(function(response){
+			return response.json();
+		})
+		.then(function(posts){
+			posts.forEach(function(post){
+				var li = $('<li></li>');
+				li.text(post.title);
+				$("#posts").append(li);
+			})
+
+			return fetch('https://jsonplaceholder.typicode.com/albums?userId=' + posts[0].userId)
+		})
+		.then(function(response){
+			return response.json();
+		})
+		.then(function(albums){
+			albums.forEach(function(album){
+				var li = $('<li></li>');
+				li.text(album.title);
+				$('#albums').append(li);
+			})
 		})
 		.catch(function(error){
 			alert(error);
 		})
-});
+})
